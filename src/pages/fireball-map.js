5ce47231571d3b621c3d1c6362f34dc090915ed4
drknozan <drknozan@gmail.com>
2023-03-01 11:36:39 +0300

    refactor: fetch the data on server side

--- src/pages/fireball-map.js
+++ src/pages/fireball-map.js
@@ -4,29 +4,38 @@ import MapContainer from '@/containers/MapContainer';
 import formatCoordinates from '@/helpers/formatCoordinates';
 import { useEffect, useState } from 'react';
 
-export default function FireballMap() {
+export default function FireballMap({ coordinates }) {
   const [loading, setLoading] = useState(true);
-  const [coordinates, setCoordinates] = useState(null);
 
   useEffect(() => {
-    fetch(
-      'https://ssd-api.jpl.nasa.gov/fireball.api?date-min=2023-01-01&req-loc=true',
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const formattedData = formatCoordinates(data.data);
-        setCoordinates(formattedData);
-        setLoading(false);
-      });
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
 
   if (loading) {
     return <LoadingSpinner />;
   }
 
-  if (!coordinates) {
-    return <Error />;
+  return <MapContainer coordinates={coordinates} />;
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://ssd-api.jpl.nasa.gov/fireball.api?date-min=2023-01-01&req-loc=true',
+  );
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch fireballs data.');
   }
 
-  return <MapContainer coordinates={coordinates} />;
+  const coordinates = formatCoordinates(data.data);
+
+  return {
+    props: {
+      coordinates: coordinates,
+    },
+    revalidate: 1800,
+  };
 }
